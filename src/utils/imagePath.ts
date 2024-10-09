605920fdc252d406857db70f47be21d6917ae7ce
developerachem <developerachem@gmail.com>
2024-10-09 10:31:22 +0600

    Fix Image Path

--- src/utils/imagePath.ts
+++ src/utils/imagePath.ts
@@ -1,6 +1,7 @@
 const imagePath = (file: string) => {
   const url = import.meta.env.VITE_API_URL;
-  return url.split("api")[0] + file;
+  console.log(url.split("api/")[0]);
+  return url.split("api/")[0] + file;
 };
 
 export default imagePath;
